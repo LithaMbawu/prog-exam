ed568b9028e282278b68e0812b7dae3382895f51
LithaMbawu <st10486617@rcconnect.edu.za>
2025-11-09 15:55:29 +0200

    improve hellow world

--- src/main/java/za/co/rosebankcollege/litha/examsapp/ExamsApp.java
+++ src/main/java/za/co/rosebankcollege/litha/examsapp/ExamsApp.java
@@ -11,6 +11,7 @@ package za.co.rosebankcollege.litha.examsapp;
 public class ExamsApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello!");
+        
     }
 }
